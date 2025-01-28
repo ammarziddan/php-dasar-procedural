879bb64a5a185ada517ccc76640b1c7b6171ad76
Ammar Ziddan <ammarziddan@gmail.com>
2025-01-29 00:48:12 +0700

    Rename directory: ajax to jquery

--- js/script.js
+++ js/script.js
@@ -5,11 +5,8 @@ $(function () {
         // munculkan icon loader
         $('.img-loader').show();
 
-        // // ajax menggunakan load
-        // $('#data-table').load('ajax/mahasiswa.php?keyword=' + $('#keyword').val());
-
         // menggunakan $.get()
-        $.get('ajax/mahasiswa.php?keyword=' + $('#keyword').val(), function(data) {
+        $.get('jquery/mahasiswa.php?keyword=' + $('#keyword').val(), function(data) {
 
             $('#data-table').html(data);
 
